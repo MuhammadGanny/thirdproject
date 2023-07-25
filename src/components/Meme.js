f70111851f7963a79b2a2e56aa8fc48ab837cefb
muhammadganny <mohammadganny@gmail.com>
2023-07-25 12:58:52 +0500

    header

---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Meme(){
+    <main>
+        <form>
+            <input type="text" ></input>
+            <input type="text" ></input>
+            <button>Get a new meme image 🖼</button>
+        </form>
+    </main>
+
+
+}
